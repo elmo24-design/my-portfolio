f95b1f8b44816dd2ed59357ebb1ac700daca4c9d
ElmoMahupil <eoh@misout.com>
2021-08-11 12:50:26 +0800

    8th commit
    added mobile responsiveness

--- dist/js/master.js
+++ dist/js/master.js
@@ -3,6 +3,34 @@ const backdropSnapgram = document.querySelector('.backdrop-snapgram')
 const backdropPWCC = document.querySelector('.backdrop-pwcc')
 const backdropCMS = document.querySelector('.backdrop-cmsapp')
 const backdropDevs = document.querySelector('.backdrop-devsForum')
+const closeMenuModal = document.querySelectorAll('#closeMenuModal')
+
+//hamburger menu animation
+const ham = document.querySelector('.ham')
+const closeMenu = document.querySelector('#closeMenu')
+const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.nav-links li')
+
+ham.addEventListener('click', () => {
+   navLinks.classList.toggle("open")
+   ham.style.opacity = 0
+   ham.style.pointerEvents = "none"
+   closeMenu.style.display="block"
+   links.forEach(link => {
+      link.classList.toggle('fade')
+   })
+})
+closeMenu.addEventListener('click', () => {
+   navLinks.classList.toggle("open")
+   setTimeout(() => {
+      ham.style.opacity = 1
+      ham.style.pointerEvents = "all"
+      closeMenu.style.display="none"
+   }, 1000)
+   links.forEach(link => {
+      link.classList.toggle('fade')
+   })
+})
 
 //buttons
 const snapgramBtn = document.querySelector('.snapgram-btn')
@@ -15,6 +43,16 @@ window.onload  = () => {
 }
 
 //Close Modals
+closeMenuModal.forEach(menu => {
+   menu.addEventListener('click', (e) => {
+      backdropSnapgram.style.display="none"
+      backdropPWCC.style.display="none"
+      backdropCMS.style.display="none"
+      backdropDevs.style.display="none"
+   })
+})
+
+
 backdropSnapgram.addEventListener('click', (e) => {
    if(e.target.classList.contains("backdrop")){
       backdropSnapgram.style.display="none"
